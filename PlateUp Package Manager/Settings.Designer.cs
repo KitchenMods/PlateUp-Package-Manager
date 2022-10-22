10839f2066bd70ae66c5428d70331d62bbcc2c5d
Lachlan Leone <starfluxgames@gmail.com>
2022-10-22 15:51:28 +1100

    Debug Mode Release

--- PlateUp Package Manager/Settings.Designer.cs
+++ PlateUp Package Manager/Settings.Designer.cs
@@ -126,7 +126,7 @@
 			// 
 			// button5
 			// 
-			this.button5.Location = new System.Drawing.Point(261, 417);
+			this.button5.Location = new System.Drawing.Point(470, 418);
 			this.button5.Name = "button5";
 			this.button5.Size = new System.Drawing.Size(75, 23);
 			this.button5.TabIndex = 9;
